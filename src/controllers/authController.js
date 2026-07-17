f816d2b5a47f0874e804beecbfcfd2fa0d7842c7
Sergio Shambir <realistic.sergio@gmail.com>
2026-07-17 23:28:12 +0200

    fix: address authentication and note review feedback

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -79,6 +79,20 @@ export const refreshUserSession = async (req, res, next) => {
       new Date() > new Date(session.refreshTokenValidUntil);
 
     if (isSessionExpired) {
+      await Session.deleteOne({
+        _id: session._id,
+      });
+
+      const cookieOptions = {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+      };
+
+      res.clearCookie('sessionId', cookieOptions);
+      res.clearCookie('accessToken', cookieOptions);
+      res.clearCookie('refreshToken', cookieOptions);
+
       throw createHttpError(401, 'Session token expired');
     }
 
--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -5,13 +5,14 @@ import { User } from '../models/user.js';
 
 export const authenticate = async (req, res, next) => {
   try {
-    const { accessToken } = req.cookies;
+    const { sessionId, accessToken } = req.cookies;
 
-    if (!accessToken) {
+    if (!sessionId || !accessToken) {
       throw createHttpError(401, 'Missing access token');
     }
 
     const session = await Session.findOne({
+      _id: sessionId,
       accessToken,
     });
 
